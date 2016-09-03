d7bca793b6a0e06ff2d5fefd4459aa1119ac2345
menezesandr3 <menezes.andr3@gmail.com>
2016-09-02 22:26:35 -0300

    programa funcional

---
+++ ponteiro02.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+//**********************************************************
+//             Laboratório de Algoritmos II
+// 
+//		Programa exemplo de utilização de ponteiros
+// 
+//					Autor André M. S.
+// 					Data: 02/09/2016
+//**********************************************************
+
+int main()
+{
+	// criação e inicialização das variáveis
+	int variavel_1 = 1, variavel_2 = 2;
+	char controle = 'n', descarga;
+	
+	// criação dos ponteiros
+	int *ponteiro_1, *ponteiro_2;
+	
+	// enquanto
+	while (1)
+	{
+		// inicialização dos ponteiros
+		ponteiro_1 = &variavel_1; // ponteiro_1 recebe o endereço de variavel_1
+		ponteiro_2 = &variavel_2; // ponteiro_2 recebe o endereço de variavel_2
+		
+		// exib o valor contido em cada variavel a partir do endereço contido 
+		//nos ponteiros
+		
+		printf ("\nvariavel_1 = %i", *ponteiro_1);
+		printf ("\nvariavel_2 = %i", *ponteiro_2);
+		
+		// exibe a solicitação de controle
+		printf ("\n\nDeseja finalizar o programa? (s/n)");
+		scanf ("%c", &controle);
+		
+		// se o comando indicar a finalização, finaliza o loop
+		if (controle == 's') break;
+		
+		// exibe a solicitação de um valor
+		printf ("\nDigite um valor para a variavel_1: ");
+		// efetua a leitura do valor e transfere o valor para a variavel_1
+		scanf ("%i", ponteiro_1);
+		//exibe a solicitação de um valor
+		printf ("\nDigite um valor para a variavel_2: ");
+		// efetua a leitura do valor e transfere o valor para a variavel_2
+		scanf ("%i, %c", ponteiro_2, &descarga);
+	}
+	return 0;
+}
