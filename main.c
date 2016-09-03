9a4d80e704c64ad491ab6b5b17b1c0b6ca0cfa8c
menezesandr3 <menezes.andr3@gmail.com>
2016-09-02 21:33:55 -0300

    programa funcional

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <iostream>
 
+//**********************************************************
+//             Laboratório de Algoritmos II
+// 
+//		Programa exemplo de utilização de ponteiros
+// 
+//					Autor André M. S.
+// 					Data: 02/09/2016
+//**********************************************************
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int numero = 2;
+int *ponteiro_numero = &numero;
+
 int main(int argc, char** argv) {
+	
+	printf ("Insira um numero: ");
+	
+	//scanf ("%i", &numero);
+	scanf ("%i", ponteiro_numero);
+	
+	//printf ("O numero inserido foi: %i!\n", numero);
+	printf ("O numero inserido foi: %i!\n", *ponteiro_numero);
+	
 	return 0;
 }
