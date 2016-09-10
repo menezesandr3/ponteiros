4842001f1816c83261271e6e2e7eb40489573ed0
menezesandr3 <menezes.andr3@gmail.com>
2016-09-09 22:19:45 -0300

    Programa Funcional

--- usando_string.c
+++ usando_string.c
@@ -9,24 +9,38 @@
 // 					Data: 09/09/2016
 //**********************************************************
 
+typedef struct 
+{
+	int indice;
+	char nome [50];
+	char telefone [15];
+} entrada;
+
 int main()
 {
 	// criação e inicialização das variáveis e ponteiros
-	char minha_string [] = {"1234567890"};
-	//char minha_string [] = {'1','2','3','4','5','6','7','8','9','0'};
-	char *meu_ponteiro = &minha_string;
+	entrada pagina_01;
+	entrada *ptr = &pagina_01;
 	
-	//exibe o valor contido em cada posição da string a partir do endereço 
-	// contido no ponteiro
-	do
-	{
-		printf ("\n O meu_ponteiro aponta para o valor: %c\n", *meu_ponteiro);
-		meu_ponteiro++;
-	}
-	 
-	while (*meu_ponteiro != 0);
+	//pagina_01.indice = 1;
+	//pagina_01.nome = {"Andre Menezes da Silva"};
+	//pagina_01.telefone = {"(55) 9687-5302"};
 	
-	printf ("\n O tamanho da minha_string eh: %i\n", sizeof (minha_string));
-	 
+	printf ("\nInsira a Entrada: ");
+	//scanf ("%i", &pagina_01.indice);
+	scanf ("%i", (*ptr).indice);
+	
+	printf ("\nInsira o Nome: ");
+	scanf ("%s", &pagina_01.nome);
+
+	printf ("\nInsira ao Telefone: ");
+	scanf ("%s", &pagina_01.telefone);
+	
+	printf ("\nEntrada: %i\n", pagina_01.indice);
+	printf ("\nNome: %s\n", pagina_01.nome);
+	printf ("\nNome: %s", ptr-> nome);
+	printf ("\nTelefone: %s\n", pagina_01.telefone);
+
+	//retorna 0
 	return 0;
 }
